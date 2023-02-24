a487733e66122baa081c65bca37313e067784ccb
jklmgoog <93127108+jklmgoog@users.noreply.github.com>
2023-02-24 14:05:23 +0800

    Create shardeum.sh

---
+++ shardeum.sh
@@ -0,0 +1,13 @@
+sudo apt update && apt upgrade -y
+sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get update
+sudo apt install docker-compose
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
+cd .shardeum
+./shell.sh
